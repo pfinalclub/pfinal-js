5183a279b6476c4a359b788b024fefaf40a6d36c
南丞 <Lampxiezi@163.com>
2018-12-26 17:12:45 +0800

    优化切片上传

--- pfinaljs.js
+++ pfinaljs.js
@@ -139,56 +139,59 @@ define(['jquery', 'lodash'], function ($, _) {
             }
             // 文件切片上传
             ,
-        upload_section: function (el, url, callback, el_btn) {
-                require(['jquery', 'bootstrap'], function () {
-                    var page = {
-                        init: function (callback) {
-                            if (el_btn) {
-                                $(el_btn).click($.proxy(this.upload, this));
-                            } else {
-                                $(el).change($.proxy(this.upload, this, callback));
-                            }
-                        },
-                        upload: function (callback) {
-                            var file = $("#file")[0].files[0], //文件对象
-                                name = file.name, //文件名
-                                size = file.size, //总大小
-                                succeed = 0;
-
-                            var shardSize = 2 * 1024 * 1024, //以2MB为一个分片
-                                shardCount = Math.ceil(size / shardSize); //总片数
-
-                            for (var i = 0; i < shardCount; ++i) {
-                                //计算每一片的起始与结束位置
-                                var start = i * shardSize,
-                                    end = Math.min(size, start + shardSize);
-
-                                //构造一个表单，FormData是HTML5新增的
-                                var form = new FormData();
-                                form.append("data", file.slice(start, end)); //slice方法用于切出文件的一部分
-                                form.append("name", name);
-                                form.append("total", shardCount); //总片数
-                                form.append("index", i + 1); //当前是第几片
-
-                                //Ajax提交
-                                $.ajax({
-                                    url: url,
-                                    type: "POST",
-                                    data: form,
-                                    async: true, //异步
-                                    processData: false, //很重要，告诉jquery不要对form进行处理
-                                    contentType: false, //很重要，指定为false才能形成正确的Content-Type
-                                    success: function (res) {
-                                        ++succeed
-                                        callback && callback(res)
-                                    }
-                                });
-                            }
+        // 文件切片上传
+        , upload_section: function(el,url,callback,el_btn,_token){
+            require(['jquery','bootstrap'],function(){
+                var page = {
+                    init: function (callback) {
+                        if(el_btn) {
+                            $(el_btn).click($.proxy(this.upload, this));
+                        } else {
+                            $(el).change($.proxy(this.upload, this,callback)); 
                         }
-                    }
-                    page.init(callback);
-                })
-            }
+                    },
+                    upload: function (callback) {
+                        var file = $("#file")[0].files[0], //文件对象
+                            name = file.name, //文件名
+                            size = file.size, //总大小
+                            succeed = 0;
+        
+                        var shardSize = 2 * 1024 * 1024, //以2MB为一个分片
+                            shardCount = Math.ceil(size / shardSize); //总片数
+        
+                        for (var i = 0; i < shardCount; ++i) {
+                            //计算每一片的起始与结束位置
+                            var start = i * shardSize,
+                                end = Math.min(size, start + shardSize);
+        
+                            //构造一个表单，FormData是HTML5新增的
+                            var form = new FormData();
+                            form.append("data", file.slice(start, end)); //slice方法用于切出文件的一部分
+                            form.append("name", name);
+                            form.append("total", shardCount); //总片数
+                            form.append("index", i + 1); //当前是第几片
+							if(_token) {
+								form.append("_token",_token)
+							}
+                            //Ajax提交
+                            $.ajax({
+                                url:url,
+                                type: "POST",
+                                data: form,
+                                async: false, //异步
+                                processData: false, //很重要，告诉jquery不要对form进行处理
+                                contentType: false, //很重要，指定为false才能形成正确的Content-Type
+                                success: function (res) {
+                                    ++succeed
+                                    callback && callback(res,succeed,shardCount)
+                                }
+                            });
+                        }
+                    }  
+                }
+                page.init(callback);
+            })
+        }
 
             //设备检测
             ,
